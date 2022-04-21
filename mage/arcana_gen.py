2f47a0a71794da377a99b7e5c282653390964bdf
Fred Williamson <83939529+williamsonf@users.noreply.github.com>
2022-04-21 12:53:39 -0400

    Add files via upload
    Revamped arcana generation for more realistic numbers

--- mage/arcana_gen.py
+++ mage/arcana_gen.py
@@ -11,11 +11,16 @@ def gen(path, gnosis):
                  'Moros' : ['Matter', 'Death'],
                  'Obrimos' : ['Forces', 'Prime'],
                  'Thyrsus' : ['Spirit', 'Life']}
+    minor_list = {'Acanthus' : 'Forces',
+                 'Mastigos' : 'Matter',
+                 'Moros' : 'Spirit',
+                 'Obrimos' : 'Death',
+                 'Thyrsus' : 'Mind'}
     arcana_list = ['Fate', 'Time', 'Mind', 'Space', 'Matter', 'Death', 'Forces', 'Prime', 'Spirit', 'Life']
     arcana_list = sorted(arcana_list)
     non_ruling = [] #we now identify all the non-ruling arcana
     for field in arcana_list:
-        if field not in path_list[path]:
+        if field not in path_list[path] and field not in minor_list[path]:
             non_ruling.append(field)
     arcana_max = get_arcana_max(gnosis) #May have one arcana at this value
     other_max = get_other_max(gnosis) #May have any other arcana at this value
@@ -26,10 +31,24 @@ def gen(path, gnosis):
         results[n] = 2
     results[random.choice(path_list[path])] += 1 #we randomly increase one to 3
     results[random.choice(non_ruling)] = 1 #we give them 1 point in a non-ruling arcana
+    non_ruling.append(minor_list[path])
     if gnosis > 1: #if gnosis is 1, then we've already finished
-        points = gnosis * 2
+        points = (gnosis-1) * 2
         while points > 0:
-            choice = random.choice(arcana_list)
+            arcana_weights = []
+            for n in arcana_list:
+                if n in path_list[path]:
+                    arcana_weights.append(results[n]*1.30)
+                elif n in minor_list[path]:
+                    if results[n] > 0:
+                        arcana_weights.append(results[n]/1.30)
+                    else:
+                        arcana_weights.append(0.5)
+                elif results[n] > 0:
+                    arcana_weights.append(results[n])
+                else:
+                    arcana_weights.append(0.75)
+            choice = random.choices(population=arcana_list, weights=arcana_weights)[0]
             if results[choice] < other_max or (results[choice] < arcana_max and check_max(arcana_max, results) == 0):
                 results[choice] += 1
                 points -= 1
